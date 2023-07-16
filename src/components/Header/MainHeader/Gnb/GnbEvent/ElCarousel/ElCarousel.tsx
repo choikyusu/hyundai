7577b919b5b75c0b9bcec007bd1c3a1c9c5ef0f2
gschoi <kyusu88@naver.com>
2023-07-16 23:16:36 +0900

    feat: carousel에 애니메이션 효과 추가

--- src/components/Header/MainHeader/Gnb/GnbEvent/ElCarousel/ElCarousel.tsx
+++ src/components/Header/MainHeader/Gnb/GnbEvent/ElCarousel/ElCarousel.tsx
@@ -1,27 +1,63 @@
 import { styled } from 'styled-components';
 import { ElCarouselItem } from './ElCarouselItem/ElCarouselItem';
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { eventList } from '../data/data';
 import { Indicator } from './Indicator/Indicator';
 
 export const ElCarousel = () => {
-  const carouselContainerRef = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(0);
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const itemList: HTMLDivElement[] = [];
 
   const onClickDot = (index: number) => {
     setSelectedIndex(index);
+
+    itemList.forEach((item, itemIndex) => {
+      if (index === itemIndex || itemIndex === selectedIndex) {
+        item.style.transition = 'transform 0.3s ease-in-out';
+      } else {
+        item.style.transition = '';
+      }
+
+      if (itemIndex === 0 && index === itemList.length - 1)
+        item.style.transform = `translateX(${width}px) scale(1)`;
+      else if (itemIndex === itemList.length - 1 && index === 0)
+        item.style.transform = `translateX(${-width}px) scale(1)`;
+      else {
+        item.style.transform = `translateX(${
+          (itemIndex - index) * width
+        }px) scale(1)`;
+      }
+    });
   };
+
+  useEffect(() => {
+    itemList.forEach((item, itemIndex) => {
+      if (itemIndex === 0 && selectedIndex === itemList.length - 1)
+        item.style.transform = `translateX(${width}px) scale(1)`;
+      else if (itemIndex === itemList.length - 1 && selectedIndex === 0)
+        item.style.transform = `translateX(${-width}px) scale(1)`;
+      else {
+        item.style.transform = `translateX(${
+          (itemIndex - selectedIndex) * width
+        }px) scale(1)`;
+      }
+    });
+  }, [width]);
 
   return (
     <Styled.ElCarousel>
-      <Styled.ElCarouselContainer ref={carouselContainerRef}>
+      <Styled.ElCarouselContainer
+        ref={(el: any) => setWidth(el?.offsetWidth || 0)}
+      >
         <Styled.ElCarouselLayer>
           {eventList.map((event, index) => (
             <ElCarouselItem
               event={event}
               index={index}
               selectedIndex={selectedIndex}
-              carouselContainerRef={carouselContainerRef}
+              width={width}
+              itemList={itemList}
             />
           ))}
         </Styled.ElCarouselLayer>
