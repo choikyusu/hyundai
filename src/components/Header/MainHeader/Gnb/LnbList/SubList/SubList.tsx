d78bb6c9abd624dda81884dc73531ddf09da5bdd
최규수 (gyusu choi) 과장 (Application SW팀) <gschoi1@seegene.com>
2023-07-25 11:07:31 +0900

    feat: 반응형 헤더 구현중 lnb list쪽

--- src/components/Header/MainHeader/Gnb/LnbList/SubList/SubList.tsx
+++ src/components/Header/MainHeader/Gnb/LnbList/SubList/SubList.tsx
@@ -33,7 +33,7 @@ export const SubList = ({
   };
 
   return (
-    <Styled.SubWrapper>
+    <Styled.SubWrapper $isShow={isShow}>
       <Styled.SubContents $isShow={isShow}>
         <Styled.SubList>
           <Styled.ModelInfo>
@@ -59,39 +59,131 @@ export const SubList = ({
 };
 
 const Styled = {
-  SubWrapper: styled.div``,
+  SubWrapper: styled.div<{ $isShow: boolean }>`
+    @media screen and (min-width: 768px) {
+      overflow-y: overlay;
+      display: none;
+      position: absolute;
+      top: 80px;
+      left: 0;
+      right: 0;
+      z-index: 99;
+      background-color: #f6f3f2;
+      border-top: 1px solid #002c5f;
+      border-bottom: 1px solid #e5e5e5;
+    }
+
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+      top: 64px;
+      max-height: calc(100vh - 60px) !important;
+    }
+
+    @media screen and (min-width: 768px) {
+      display: ${props => (props.$isShow ? 'block' : 'none')};
+      max-height: calc(100vh - 80px);
+    }
+  `,
   SubContents: styled.div<{ $isShow: boolean }>`
-    display: ${props => (props.$isShow ? 'block' : 'none')};
+    @media screen and (min-width: 768px) {
+      position: relative;
+      z-index: 4;
+      margin: 0 auto;
+      padding: 32px 30px 38px;
+      max-width: 1400px;
+      box-sizing: content-box;
+    }
+
+    @media screen and (max-width: 767px) {
+      display: ${props => (props.$isShow ? 'block' : 'none')};
+    }
   `,
   SubList: styled.div``,
   ModelInfo: styled.div`
-    padding-left: 0 !important;
+    @media screen and (max-width: 767px) {
+      padding-left: 0 !important;
+    }
+
+    @media screen and (min-width: 768px) {
+      position: relative;
+      z-index: 3;
+      padding-left: 0;
+    }
+
     height: auto;
     min-height: 200px;
   `,
   SubMenuList: styled.ul`
-    padding-left: 0;
+    @media screen and (max-width: 767px) {
+      padding-left: 0;
+    }
   `,
   SubMenuItem: styled.li`
-    position: relative;
-    background-color: #f6f3f2;
+    @media screen and (min-width: 768px) {
+      list-style: none;
+    }
+
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+      width: 140px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+      width: 165px;
+    }
+
+    @media screen and (max-width: 767px) {
+      position: relative;
+      background-color: #f6f3f2;
+    }
   `,
   ModelButton: styled(CommonStyled.Button)<{ $isSelected: boolean }>`
-    display: block;
-    position: relative;
-    width: 100%;
-    padding: 0 15px 0 30px;
-    background-color: #f6f3f2;
-    border-bottom: 1px solid #e5e5e5;
-    font-family: 'HyundaiSansHeadKR';
-    font-size: 15px;
-    line-height: 23px;
-    letter-spacing: -0.4px;
-    line-height: 59px;
+    @media screen and (max-width: 767px) {
+      width: auto;
+      text-align: left;
+      color: ${props => (props.$isSelected ? '#007fa8;' : '#000')};
+      display: block;
+    }
 
-    width: auto;
-    text-align: left;
-    color: ${props => (props.$isSelected ? '#007fa8;' : '#000')};
-    display: block;
+    @media screen and (max-width: 767px) {
+      display: block;
+      position: relative;
+      width: 100%;
+      padding: 0 15px 0 30px;
+      background-color: #f6f3f2;
+      border-bottom: 1px solid #e5e5e5;
+      font-family: 'HyundaiSansHeadKR';
+      font-size: 15px;
+      line-height: 23px;
+      letter-spacing: -0.4px;
+      line-height: 59px;
+    }
+
+    @media screen and (min-width: 768px) {
+      outline: none;
+      border: none;
+    }
+
+    @media screen and (min-width: 768px) {
+      position: relative;
+      width: 18.29%;
+      padding: 8px 0;
+      display: block;
+      color: #000;
+      font-family: 'HyundaiSansHeadKR';
+      font-size: 16px;
+      line-height: 30px;
+      letter-spacing: -0.4px;
+    }
+
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+      width: 165px;
+      font-size: 15px;
+    }
+
+    @media screen and (min-width: 768px) and (max-width: 1024px) {
+      text-align: left;
+    }
   `,
 };
