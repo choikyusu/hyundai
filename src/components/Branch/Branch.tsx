b8e3188767eae4756f8c2a43fefb176b8eb121ef
최규수 <kyusu88@naver.com>
2023-09-08 15:27:22 +0900

    refactor: arrow 영역 리팩토링
    - pagination component로 따로

--- src/components/Branch/Branch.tsx
+++ src/components/Branch/Branch.tsx
@@ -2,14 +2,9 @@ import { CommonStyled } from '@/src/styles/CommonStyled';
 import Link from 'next/link';
 import { styled } from 'styled-components';
 import { Map } from '../map/Map';
-import {
-  MdKeyboardArrowLeft,
-  MdKeyboardArrowRight,
-  MdKeyboardDoubleArrowLeft,
-  MdKeyboardDoubleArrowRight,
-} from 'react-icons/md';
 import { useBranch } from './useBranch';
 import { ContentArea } from '../common/ContentArea/ContentArea';
+import { Pagination } from '../common/Pagination/Pagination';
 
 export const Branch = () => {
   const {
@@ -140,42 +135,12 @@ export const Branch = () => {
                           </Styled.ResultItem>
                         ))}
                       </Styled.ResultList>
-                      <Styled.Paging>
-                        <Styled.Pagination>
-                          <Styled.ButtonPreAll>
-                            <MdKeyboardDoubleArrowLeft
-                              onClick={() => onClickArrow('First')}
-                            />
-                          </Styled.ButtonPreAll>
-                          <Styled.ButtonPrev
-                            onClick={() => onClickArrow('Previous')}
-                          >
-                            <MdKeyboardArrowLeft />
-                          </Styled.ButtonPrev>
-                          <Styled.ElPagination>
-                            <Styled.ElPager>
-                              {pageIndexList.map(index => (
-                                <Styled.Number
-                                  $isSelected={isSelectedPage(index)}
-                                  onClick={() => onClickPageNo(index)}
-                                >
-                                  {index}
-                                </Styled.Number>
-                              ))}
-                            </Styled.ElPager>
-                          </Styled.ElPagination>
-                          <Styled.ButtonNext
-                            onClick={() => onClickArrow('Next')}
-                          >
-                            <MdKeyboardArrowRight />
-                          </Styled.ButtonNext>
-                          <Styled.ButtonNextAll
-                            onClick={() => onClickArrow('Last')}
-                          >
-                            <MdKeyboardDoubleArrowRight />
-                          </Styled.ButtonNextAll>
-                        </Styled.Pagination>
-                      </Styled.Paging>
+                      <Pagination
+                        isSelectedPage={isSelectedPage}
+                        onClickArrow={onClickArrow}
+                        onClickPageNo={onClickPageNo}
+                        pageIndexList={pageIndexList}
+                      />
                     </Styled.ScrollWrap>
                   </Styled.SelectWrap>
                 </Styled.Branch>
@@ -657,212 +622,6 @@ const Styled = {
 
     @media screen and (max-width: 1024px) and (min-width: 768px) {
       width: 64%;
-    }
-  `,
-  Paging: styled.div`
-    @media screen and (max-width: 767px) {
-      margin: 40px 0;
-      text-align: center;
-    }
-  `,
-  Pagination: styled.div`
-    text-align: center;
-  `,
-  ButtonPreAll: styled(CommonStyled.Button)`
-    svg {
-      width: 20px;
-      height: 20px;
-    }
-
-    @media screen and (max-width: 767px) {
-      display: inline-block;
-      vertical-align: middle;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: 280px;
-      height: 40px;
-      border: 0;
-      background: #002c5f;
-      font-family: 'HyundaiSansTextKR';
-      font-size: 15px;
-      font-weight: 500;
-      letter-spacing: -0.4px;
-      color: #fff;
-      cursor: pointer;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: auto;
-      height: auto;
-      border-radius: 0;
-      color: #000;
-      background-color: transparent;
-    }
-  `,
-  ButtonPrev: styled(CommonStyled.Button)`
-    svg {
-      width: 20px;
-      height: 20px;
-    }
-
-    @media screen and (max-width: 767px) {
-      display: inline-block;
-      vertical-align: middle;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: 280px;
-      height: 40px;
-      border: 0;
-      background: #002c5f;
-      font-family: 'HyundaiSansTextKR';
-      font-size: 15px;
-      font-weight: 500;
-      letter-spacing: -0.4px;
-      color: #fff;
-      cursor: pointer;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: auto;
-      height: auto;
-      border-radius: 0;
-      color: #000;
-      background-color: transparent;
-    }
-
-    @media screen and (max-width: 640px) {
-      margin-left: 10px;
-    }
-  `,
-  ButtonNextAll: styled(CommonStyled.Button)`
-    svg {
-      width: 20px;
-      height: 20px;
-    }
-
-    @media screen and (max-width: 767px) {
-      display: inline-block;
-      vertical-align: middle;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: 280px;
-      height: 40px;
-      border: 0;
-      background: #002c5f;
-      font-family: 'HyundaiSansTextKR';
-      font-size: 15px;
-      font-weight: 500;
-      letter-spacing: -0.4px;
-      color: #fff;
-      cursor: pointer;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: auto;
-      height: auto;
-      border-radius: 0;
-      color: #000;
-      background-color: transparent;
-    }
-
-    @media screen and (max-width: 640px) {
-      margin-right: 10px;
-    }
-  `,
-  ButtonNext: styled(CommonStyled.Button)`
-    svg {
-      width: 20px;
-      height: 20px;
-    }
-
-    @media screen and (max-width: 767px) {
-      display: inline-block;
-      vertical-align: middle;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: 280px;
-      height: 40px;
-      border: 0;
-      background: #002c5f;
-      font-family: 'HyundaiSansTextKR';
-      font-size: 15px;
-      font-weight: 500;
-      letter-spacing: -0.4px;
-      color: #fff;
-      cursor: pointer;
-    }
-
-    @media screen and (max-width: 767px) {
-      width: auto;
-      height: auto;
-      border-radius: 0;
-      color: #000;
-      background-color: transparent;
-    }
-  `,
-  ElPagination: styled.div`
-    white-space: nowrap;
-    padding: 2px 5px;
-    color: #303133;
-    font-weight: 700;
-
-    vertical-align: middle;
-    display: inline-block;
-    white-space: nowrap;
-
-    display: inline-block;
-    vertical-align: middle;
-  `,
-  ElPager: styled.ul`
-    user-select: none;
-    list-style: none;
-    display: inline-block;
-    vertical-align: top;
-    font-size: 0;
-    padding: 0;
-    margin: 0;
-  `,
-  Number: styled.li<{ $isSelected: boolean }>`
-    padding: 0 4px;
-    background: #fff;
-    vertical-align: top;
-    display: inline-block;
-    font-size: 13px;
-    min-width: 35.5px;
-    height: 28px;
-    line-height: 28px;
-    cursor: ${props => (props.$isSelected ? 'default' : 'pointer')};
-    color: ${props => (props.$isSelected ? '#409eff' : '#303133')};
-    box-sizing: border-box;
-    text-align: center;
-    margin: 0;
-
-    &:hover {
-      color: #409eff;
-    }
-
-    @media screen and (max-width: 640px) {
-      min-width: auto !important;
-      margin: 0 8px !important;
-      padding: 0 !important;
-    }
-
-    @media screen and (max-width: 9999px) {
-      position: relative;
-
-      padding: 0;
-
-      position: relative;
-    }
-
-    @media screen and (max-width: 640px) {
-      &:first-child {
-        margin-left: 25px;
-      }
     }
   `,
 };
